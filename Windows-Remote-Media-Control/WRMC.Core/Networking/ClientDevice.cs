aca0ae1a1544d795996513bb5b5f097553f2d87f
DerMagereStudent <malo0466@gmail.com>
2020-12-26 13:58:55 +0100

    Models for server and client added.

---
+++ Windows-Remote-Media-Control/WRMC.Core/Networking/ClientDevice.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace WRMC.Core.Networking {
+	/// <summary>
+	/// Represents all necessary information about a client.
+	/// </summary>
+	public class ClientDevice {
+		/// <summary>
+		/// The ID of the client.
+		/// </summary>
+		public string ID { get; set; }
+
+		/// <summary>
+		/// The device name of the client.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// The local IP-Addresse of the client.
+		/// </summary>
+		public IPAddress IPAddress { get; set; }
+
+		/// <summary>
+		/// The session id of the client.
+		/// </summary>
+		public long SessionID { get; set; }
+	}
+}
---
+++ Windows-Remote-Media-Control/WRMC.Core/Networking/ServerDevice.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace WRMC.Core.Networking {
+	/// <summary>
+	/// Represents all necessary information about a server.
+	/// </summary>
+	public class ServerDevice {
+		/// <summary>
+		/// The ID of the server.
+		/// </summary>
+		public string ID { get; set; }
+
+		/// <summary>
+		/// The device name of the server.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// The local IP-Addresse of the server.
+		/// </summary>
+		public IPAddress IPAddress { get; set; }
+	}
+}
